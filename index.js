781a1849d76dc4bc50a738447ae6d7b749db2dae
Lydia Scharron <lfscharr@gmail.com>
2023-08-22 01:46:31 -0400

    loop.lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function writeCards(friend, event) {
+    const thankYou = []
+    for (let i = 0; i < friend.length; i++) {
+        thankYou.push(`Thank you, ${friend[i]}, for the wonderful ${event} gift!`);
+        console.log(thankYou)
+    }
+
+    return thankYou;
+}
+
+writeCards(friend);
+
+
+function countDown(num) {
+let countDown = num ;
+while (countDown >= 0); {
+    console.log(countDown);
+    
+countDown-= 1
+}
+
+}
+
 // Code your solutions in this file
